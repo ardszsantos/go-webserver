ff14fd60d8bb850d635e00d37a4fc4282e247fb6
ardszsantos <lokgamer01@gmail.com>
2024-11-14 16:31:18 -0300

    Created the postsHandler and postHandler

--- main.go
+++ main.go
@@ -25,3 +25,33 @@ func main() {
 	fmt.Println("Server is running at http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
+
+
+func postsHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		handleGetPosts(w, r)
+	case "POST":
+		handlePostPosts(w, r)
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+}
+
+func postHandler(w http.ResponseWriter, r *http.Request) {
+	id, err ;= strconv.Atoi(r.URL.Path[len("/posts/"):])
+
+	if err != nil {
+		http.Error(w, "Invalid post ID", http.StatusBadRequest)
+		return
+	}
+
+	switch r.Method {
+	case "GET":
+		handleGetPosts(w, r, id)
+	case "DELETE":
+		handleDeletePost(w, r, id)
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+}
