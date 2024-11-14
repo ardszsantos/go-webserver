10e2fda780a21702847fc2b1ff6fa0d0831a155e
ardszsantos <lokgamer01@gmail.com>
2024-11-14 16:18:57 -0300

    boilerplate

--- main.go
+++ main.go
@@ -4,41 +4,24 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync"
 )
 
-func formHandler(w http.ResponseWriter, r *http.Request) {
-	if err := r.ParseForm(); err != nil {
-		fmt.Fprintf(w, "ParseForm() err: %v", err)
-		return
-	}
-	fmt.Fprintf(w, "POST request successful")
-	name := r.FormValue("name")
-	address := r.FormValue("address")
-	fmt.Fprintf(w, "name = %s\n", name)
-	fmt.Fprintf(w, "address = %s\n", address)
-
+type Post struct {
+	ID   int    `json:"id"`
+	Body string `json:"body"`
 }
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/hello" {
-		http.Error(w, "404 not found", http.StatusNotFound)
-		return
-	}
-	if r.Method != "GET" {
-		http.Error(w, "method is not supported", http.StatusNotFound)
-		return
-	}
-	fmt.Fprintf(w, "hello!")
-}
+var (
+	posts   = make(map[int]Post)
+	nextID  = 1
+	postsMu sync.Mutex
+)
 
 func main() {
-	fileServer := http.FileServer(http.Dir("./static"))
-	http.Handle("/", fileServer)
-	http.HandleFunc("/form", formHandler)
-	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/posts", postsHandler)
+	http.HandleFunc("/posts/", postHandler)
 
-	fmt.Printf("Starting server at port 8080\n")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal(err)
-	}
+	fmt.Println("Server is running at http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
